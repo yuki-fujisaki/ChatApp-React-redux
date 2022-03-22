7597d4e8752ffbb41e2aa7d55b2cea269514a95f
yuki-fujisaki <fuji.blue.2417@gmail.com>
2022-03-22 18:37:34 +0900

    Create App components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
+import Auth from "./components/Auth";
+import Chat from "./components/Chat";
 
-function App() {
+const App: React.FC = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
   return (
-    <div className="App">
-    </div>
+    <>
+      {user.uid ? (
+        <div className="app">
+          <Chat />
+        </div>
+      ) : (
+        <Auth />
+      )}
+    </>
   );
-}
+};
 
 export default App;
